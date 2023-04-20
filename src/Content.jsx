b8f248c9e07847af580c468310fb6c37dc9888a0
dallinforsyth <dallfor192@gmail.com>
2023-04-20 15:31:25 -0500

    const workouts

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,14 @@
 import { WorkoutsIndex } from "./WorkoutsIndex";
 
 export function Content() {
+  const workouts = [
+    { id: 1, name: "First", url: "https://via.placeholder.com/150", width: 150, height: 150 },
+    { id: 2, name: "Second", url: "https://via.placeholder.com/300", width: 300, height: 300 },
+  ];
+
   return (
     <div>
-      <WorkoutsIndex />
+      <WorkoutsIndex workouts={workouts} />
     </div>
   );
 }
