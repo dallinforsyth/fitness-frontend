a54f199236409044a8270101936dd5901223cdf6
dallinforsyth <dallfor192@gmail.com>
2023-04-20 15:38:45 -0500

    added raw data

--- src/WorkoutsIndex.jsx
+++ src/WorkoutsIndex.jsx
@@ -1,7 +1,15 @@
-export function WorkoutsIndex() {
+export function WorkoutsIndex(props) {
   return (
     <div>
       <h1>All Workouts</h1>
+      {props.workouts.map((workout) => (
+        <div key={workout.id}>
+          <h2>{workout.name}</h2>
+          <img src={workout.url} />
+          <p>Width: {workout.width}</p>
+          <p>Height: {workout.height}</p>
+        </div>
+      ))}
     </div>
   );
 }
