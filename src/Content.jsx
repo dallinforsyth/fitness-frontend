7ef28dd61eaf2d83900133f21dad2bd671a891b8
dallinforsyth <dallfor192@gmail.com>
2023-04-26 12:16:33 -0500

    handle index workouts

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,10 +3,17 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 export function Content() {
-  const workouts = [
-    { id: 1, name: "First", url: "https://via.placeholder.com/150", width: 150, height: 150 },
-    { id: 2, name: "Second", url: "https://via.placeholder.com/300", width: 300, height: 300 },
-  ];
+  const [workouts, setWorkouts] = useState([]);
+
+  const handleIndexWorkouts = () => {
+    console.log("handleIndexWorkouts");
+    axios.get("http://localhost:3000/workouts.json").then((response) => {
+      console.log(response.data);
+      setWorkouts(response.data);
+    });
+  };
+
+  useEffect(handleIndexWorkouts, []);
 
   return (
     <div>
