89cf8f424eac6fab3e4d3c6c54b66280923d36ac
dallinforsyth <dallfor192@gmail.com>
2023-05-08 14:12:19 -0500

    model css

--- src/WorkoutsIndex.jsx
+++ src/WorkoutsIndex.jsx
@@ -5,9 +5,7 @@ export function WorkoutsIndex(props) {
       {props.workouts.map((workout) => (
         <div key={workout.id}>
           <h2>{workout.name}</h2>
-          <img src={workout.url} />
-          <p>Width: {workout.width}</p>
-          <p>Height: {workout.height}</p>
+          <img src={workout.img} />
         </div>
       ))}
     </div>
