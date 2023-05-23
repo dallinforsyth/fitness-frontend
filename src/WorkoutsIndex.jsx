6b454ffd2bd5e7800c7f4d245ef130ffbdb5d03e
dallinforsyth <dallfor192@gmail.com>
2023-05-23 17:12:23 -0500

    ui

--- src/WorkoutsIndex.jsx
+++ src/WorkoutsIndex.jsx
@@ -1,7 +1,22 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
 export function WorkoutsIndex(props) {
   return (
     <div>
       <h1>All Workouts</h1>
+      <div>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
       {props.workouts.map((workout) => (
         <div key={workout.id}>
           <h2>{workout.name}</h2>
