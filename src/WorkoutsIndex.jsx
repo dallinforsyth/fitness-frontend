d16e07fb553cc8dc47109b70a9095ec731117b46
dallinforsyth <dallfor192@gmail.com>
2023-05-24 16:20:03 -0500

    name

--- src/WorkoutsIndex.jsx
+++ src/WorkoutsIndex.jsx
@@ -6,7 +6,7 @@ export function WorkoutsIndex(props) {
     <div>
       <h1>All Workouts</h1>
       <div>
-        <Card style={{ width: "18rem" }}>
+        {/* <Card style={{ width: "18rem" }}>
           <Card.Img variant="top" src="holder.js/100px180" />
           <Card.Body>
             <Card.Title>Card Title</Card.Title>
@@ -15,11 +15,23 @@ export function WorkoutsIndex(props) {
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
-        </Card>
+        </Card> */}
       </div>
       {props.workouts.map((workout) => (
         <div key={workout.id}>
-          <h2>{workout.name}</h2>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>
+                <h2>{workout.name}</h2>
+              </Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          {/* <h2>{workout.name}</h2> */}
           <img src={workout.img} />
           <button onClick={() => props.onShowWorkout(workout)}>More info</button>
         </div>
