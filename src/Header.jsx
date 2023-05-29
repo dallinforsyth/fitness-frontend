9373a5efa535f213ca09471a922e19cf76aa3fe3
dallinforsyth <dallfor192@gmail.com>
2023-05-29 13:37:11 -0500

    names

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,11 +8,11 @@ export function Header() {
       <nav>
         <Navbar bg="dark" variant="dark" fixed="top">
           <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar.Brand href="#home">Home</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="#home">Your Workouts</Nav.Link>
+              <Nav.Link href="#pricing">Your Account</Nav.Link>
+              <Nav.Link href="#features">Pricing</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
