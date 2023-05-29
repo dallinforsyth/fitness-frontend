e0df759e7ef904ab261cf61957a41aa4725e3e91
dallinforsyth <dallfor192@gmail.com>
2023-05-29 14:22:20 -0500

    welcome

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,7 +10,7 @@ export function Header() {
         <Carousel.Item>
           <img className="d-block w-100" src="holder.js/800x400?text=First slide&bg=f5f5f5" alt="First slide" />
           <Carousel.Caption>
-            <h5>First slide label</h5>
+            <h5>Welcome</h5>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
