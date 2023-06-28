02b44eb66cc164a2d769ea136709d966da97b382
dallinforsyth <dallfor192@gmail.com>
2023-06-27 22:02:17 -0500

    pricing

--- src/Content.jsx
+++ src/Content.jsx
@@ -8,6 +8,7 @@ import { LogoutLink } from "./LogoutLink";
 import { Login } from "./Login";
 import { Signup } from "./Signup";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Pricing } from "./Pricing";
 
 export function Content() {
   return (
@@ -17,6 +18,7 @@ export function Content() {
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<LogoutLink />} />
         <Route path="/" element={<WorkoutsIndex />} />
+        <Route path="/pricing" element={<Pricing />} />
       </Routes>
       {/* <WorkoutsIndex workouts={workouts} onShowWorkout={handleShowWorkout} /> */}
     </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -49,8 +49,8 @@ export function Header() {
                 <Navbar.Brand href="/">Home</Navbar.Brand>
                 <Nav className="me-auto">
                   <Nav.Link href="/">Your Workouts</Nav.Link>
-                  <Nav.Link href="/pricing">Your Account</Nav.Link>
-                  <Nav.Link href="#features">Pricing</Nav.Link>
+                  <Nav.Link href="#pricing">Your Account</Nav.Link>
+                  <Nav.Link href="/pricing">Pricing</Nav.Link>
                   <Nav.Link href="/login">Login</Nav.Link>
                   <Nav.Link href="/logout">Logout</Nav.Link>
                   <Nav.Link href="/signup">Signup</Nav.Link>
