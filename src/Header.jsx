d7e1a210ca5b456a1c716afdbd2b9f080cd9dfa3
dallinforsyth <dallfor192@gmail.com>
2023-06-28 18:04:53 -0500

    size

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ export function Header() {
   return (
     <div>
       <>
-        <Carousel variant="dark" aria-setsize={10}>
+        <Carousel variant="dark" aria-setsize={1}>
           <Carousel.Item>
             <img
               className="d-block w-100"
