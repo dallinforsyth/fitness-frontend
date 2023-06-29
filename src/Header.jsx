3465b5e403d830f2e9532ee7047808a8ab4936c8
dallinforsyth <dallfor192@gmail.com>
2023-06-29 13:24:01 -0500

    images size

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Carousel from "react-bootstrap/Carousel";
+import "./Header.css";
 
 export function Header() {
   return (
@@ -10,7 +11,6 @@ export function Header() {
         <Carousel variant="dark" className="mh-100">
           <Carousel.Item>
             <img
-              style={{ maxHeight: "600px", objectFit: "contain" }}
               className="d-block w-100 my-auto img-fluid"
               src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/rockcms/2021-12/211208-working-out-stock-mn-1310-55e1c7.jpg"
               alt="First slide"
@@ -22,7 +22,7 @@ export function Header() {
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-50"
+              className="d-block w-100 my-auto img-fluid"
               src="https://media.istockphoto.com/id/615883260/photo/difficult-doesnt-mean-impossible.jpg?s=612x612&w=0&k=20&c=cAEJvjTFRuF9H9gRov1Aj4X4I6xV6DSvMwWsf-2IW-0="
               alt="Second slide"
             />
@@ -33,7 +33,7 @@ export function Header() {
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-50"
+              className="d-block w-100 my-auto img-fluid"
               src="https://goldsgym.in/uploads/blog/compress-strong-man-training-gym-min.jpg"
               alt="Third slide"
             />
