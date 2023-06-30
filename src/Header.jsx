f7a38efa83330351b7aa79d7a5d6aa3f28da25b4
dallinforsyth <dallfor192@gmail.com>
2023-06-30 15:42:04 -0500

    href

--- src/Header.jsx
+++ src/Header.jsx
@@ -50,7 +50,7 @@ export function Header() {
                 <Navbar.Brand href="/">Home</Navbar.Brand>
                 <Nav className="me-auto">
                   <Nav.Link href="/">Your Workouts</Nav.Link>
-                  <Nav.Link href="#pricing">Your Account</Nav.Link>
+                  <Nav.Link href="/youraccount">Your Account</Nav.Link>
                   <Nav.Link href="/pricing">Pricing</Nav.Link>
                   <Nav.Link href="/login">Login</Nav.Link>
                   <Nav.Link href="/logout">Logout</Nav.Link>
---
+++ src/YourAccount.jsx
@@ -0,0 +1,7 @@
+export function YourAccount() {
+  return (
+    <footer>
+      <p>Copyright 2022</p>
+    </footer>
+  );
+}
