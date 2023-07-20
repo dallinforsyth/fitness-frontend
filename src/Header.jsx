624c91d166d462ad3d76c2248814c5c48d08f99d
dallinforsyth <dallfor192@gmail.com>
2023-07-20 16:10:50 -0500

    logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -14,6 +14,7 @@ export function Header() {
         <Row>
           <Col style={{ fontSize: 33 }}>
             <p className="my-1 mx-4">FitZone Fitness</p>
+            <img src="./ff.png" className="App-logo" alt="logo" />
           </Col>
         </Row>
       </Container>
