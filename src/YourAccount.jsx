297f281d53615ce67eae665607163757c7aa343d
dallinforsyth <dallfor192@gmail.com>
2023-07-29 15:23:12 -0500

    user info

--- src/YourAccount.jsx
+++ src/YourAccount.jsx
@@ -1,25 +1,60 @@
-import { useReducer } from "react";
+// import { useReducer } from "react";
+// import axios from "axios";
+// import { useEffect } from "react";
+
+// export function YourAccount() {
+//   const getUser = () => {
+//     axios.get("http://localhost:3000/users/2.json").then((response) => {
+//       console.log(response.data);
+//     });
+//   };
+
+//   useEffect(() => {
+//     getUser();
+//   });
+
+//   // const [workouts, setWorkouts] = useState([]);
+
+//   return (
+//     <footer>
+//       /* <p> {user.name} </p>
+//       <p> {user.email} </p> */
+//       <p>Copyright 2022</p>
+//     </footer>
+//   );
+// }
+
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { useEffect } from "react";
 
 export function YourAccount() {
-  const getUser = () => {
-    axios.get("http://localhost:3000/users/2.json").then((response) => {
-      console.log(response.data);
-    });
-  };
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
-    getUser();
-  });
+    // Make the API call to fetch user data
+    axios
+      .get(`http://localhost:3000/users/2.json`)
+      .then((response) => {
+        setUserData(response.data); // Update the state with user data
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
+      });
+  }, []);
 
-  // const [workouts, setWorkouts] = useState([]);
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <footer>
-      {/* <p> {user.name} </p>
-      <p> {user.email} </p> */}
-      <p>Copyright 2022</p>
-    </footer>
+    <div>
+      <h2>User Information</h2>
+      <p>
+        <strong>Name:</strong> {userData.name}
+      </p>
+      <p>
+        <strong>Email:</strong> {userData.email}
+      </p>
+    </div>
   );
 }
